35d921a4382c408cec46567819fd4051daff0482
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2023-03-15 22:47:19 -0500

    Incorporando pruebas rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/health', to: 'health#health'
+
+  resources :posts, only: [:index, :show]
 end
